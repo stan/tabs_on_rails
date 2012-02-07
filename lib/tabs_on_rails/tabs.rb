ed2136b458f97d3d4184c809dbbcb2cf4e1a38c2
stan Mazurek <stan@mazurek.me>
2012-02-08 04:01:23 +0900

    Shambles

--- lib/tabs_on_rails/tabs.rb
+++ lib/tabs_on_rails/tabs.rb
@@ -9,7 +9,7 @@
 
 require 'tabs_on_rails/tabs/builder'
 require 'tabs_on_rails/tabs/tabs_builder'
-
+require 'tabs_on_rails/tabs/navbar_tabs_builder'
 
 module TabsOnRails
 
@@ -17,7 +17,7 @@ module TabsOnRails
 
     def initialize(context, options = {})
       @context = context
-      @builder = (options.delete(:builder) || TabsBuilder).new(@context, options)
+      @builder = (options.delete(:builder) || NavbarTabsBuilder).new(@context, options)
       @options = options
     end
 
@@ -33,9 +33,15 @@ module TabsOnRails
     end
 
     def method_missing(*args, &block)
-      @builder.tab_for(*args, &block)
+      case args.first
+      when :divider
+        @builder.divider(*args, &block)
+      when :nolink
+        @builder.nolink(*args, &block)
+      else
+        @builder.tab_for(*args, &block)
+      end
     end
-
 
     # Renders the tab stack using the current builder.
     #
@@ -44,13 +50,18 @@ module TabsOnRails
       raise LocalJumpError, "no block given" unless block_given?
 
       options = @options.dup
+      dropdown = options.delete(:dropdown)
       open_tabs_options  = options.delete(:open_tabs)  || {}
+      open_tabs_options.merge! :class => "dropdown-menu" if dropdown
       close_tabs_options = options.delete(:close_tabs) || {}
 
       "".tap do |html|
+        html << @context.tag(:li, :class => "dropdown") if dropdown
+        html << @context.link_to(dropdown[:name], '#', :class => "dropdown-toggle") if dropdown
         html << open_tabs(open_tabs_options).to_s
         html << @context.capture(self, &block)
         html << close_tabs(close_tabs_options).to_s
+        html << "</li>" if dropdown
       end.html_safe
     end
 
